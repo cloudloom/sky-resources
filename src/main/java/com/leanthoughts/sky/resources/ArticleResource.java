b27d85334124a77fd66334136c6105c5e530cf7a
Fahad Fazil <fahad.fazil007@gmail.com>
2016-01-14 00:20:52 +0530

    Added Id and imageId in ArticleResource

--- src/main/java/com/leanthoughts/sky/resources/ArticleResource.java
+++ src/main/java/com/leanthoughts/sky/resources/ArticleResource.java
@@ -11,6 +11,8 @@ public class ArticleResource extends ProductResource {
     private String ean;
     private String longDescription;
     private PriceResource price;
+    private String image;
+    private String id;
     private Set<CategoryResource> categories = new HashSet<CategoryResource>(0);
 
     public ArticleResource() {
@@ -56,26 +58,45 @@ public class ArticleResource extends ProductResource {
         this.categories = categories;
     }
 
+    public String getImage(){return this.image;}
+
+    public String getId(){return this.id;}
+
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public boolean equals(Object o) {
         if (o == this) return true;
         if (!(o instanceof ArticleResource)) return false;
         final ArticleResource other = (ArticleResource) o;
         if (!other.canEqual((Object) this)) return false;
-        final Object this$sku = this.sku;
-        final Object other$sku = other.sku;
+        final Object this$sku = this.getSku();
+        final Object other$sku = other.getSku();
         if (this$sku == null ? other$sku != null : !this$sku.equals(other$sku)) return false;
-        final Object this$ean = this.ean;
-        final Object other$ean = other.ean;
+        final Object this$ean = this.getEan();
+        final Object other$ean = other.getEan();
         if (this$ean == null ? other$ean != null : !this$ean.equals(other$ean)) return false;
-        final Object this$longDescription = this.longDescription;
-        final Object other$longDescription = other.longDescription;
+        final Object this$longDescription = this.getLongDescription();
+        final Object other$longDescription = other.getLongDescription();
         if (this$longDescription == null ? other$longDescription != null : !this$longDescription.equals(other$longDescription))
             return false;
-        final Object this$price = this.price;
-        final Object other$price = other.price;
+        final Object this$price = this.getPrice();
+        final Object other$price = other.getPrice();
         if (this$price == null ? other$price != null : !this$price.equals(other$price)) return false;
-        final Object this$categories = this.categories;
-        final Object other$categories = other.categories;
+        final Object this$image = this.getImage();
+        final Object other$image = other.getImage();
+        if (this$image == null ? other$image != null : !this$image.equals(other$image)) return false;
+        final Object this$id = this.getId();
+        final Object other$id = other.getId();
+        if (this$id == null ? other$id != null : !this$id.equals(other$id)) return false;
+        final Object this$categories = this.getCategories();
+        final Object other$categories = other.getCategories();
         if (this$categories == null ? other$categories != null : !this$categories.equals(other$categories))
             return false;
         return true;
@@ -84,15 +105,19 @@ public class ArticleResource extends ProductResource {
     public int hashCode() {
         final int PRIME = 59;
         int result = 1;
-        final Object $sku = this.sku;
+        final Object $sku = this.getSku();
         result = result * PRIME + ($sku == null ? 0 : $sku.hashCode());
-        final Object $ean = this.ean;
+        final Object $ean = this.getEan();
         result = result * PRIME + ($ean == null ? 0 : $ean.hashCode());
-        final Object $longDescription = this.longDescription;
+        final Object $longDescription = this.getLongDescription();
         result = result * PRIME + ($longDescription == null ? 0 : $longDescription.hashCode());
-        final Object $price = this.price;
+        final Object $price = this.getPrice();
         result = result * PRIME + ($price == null ? 0 : $price.hashCode());
-        final Object $categories = this.categories;
+        final Object $image = this.getImage();
+        result = result * PRIME + ($image == null ? 0 : $image.hashCode());
+        final Object $id = this.getId();
+        result = result * PRIME + ($id == null ? 0 : $id.hashCode());
+        final Object $categories = this.getCategories();
         result = result * PRIME + ($categories == null ? 0 : $categories.hashCode());
         return result;
     }
@@ -102,6 +127,6 @@ public class ArticleResource extends ProductResource {
     }
 
     public String toString() {
-        return "com.leanthoughts.sky.resources.ArticleResource(sku=" + this.sku + ", ean=" + this.ean + ", longDescription=" + this.longDescription + ", price=" + this.price + ", categories=" + this.categories + ")";
+        return "com.leanthoughts.sky.resources.ArticleResource(sku=" + this.getSku() + ", ean=" + this.getEan() + ", longDescription=" + this.getLongDescription() + ", price=" + this.getPrice() + ", image=" + this.getImage() + ", id=" + this.getId() + ", categories=" + this.getCategories() + ")";
     }
 }
