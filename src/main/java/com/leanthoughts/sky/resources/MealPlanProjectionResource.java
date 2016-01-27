6197b997ab91a1f110db3ea3bb376c07e6b1845f
sadath <ssmdrepo@gmail.com>
2016-01-27 11:28:32 +0530

    Changed To v1.8 Added MealPlanProjectionResource

--- src/main/java/com/leanthoughts/sky/resources/MealPlanProjectionResource.java
+++ src/main/java/com/leanthoughts/sky/resources/MealPlanProjectionResource.java
@@ -8,6 +8,8 @@ import java.util.Set;
  */
 public class MealPlanProjectionResource extends BaseResource {
     private String flight;
+    private StationResource upliftStation;
+    private StationResource unloadStation;
     private MealPlanResource mealPlan;
     private Set<MealPlanProjectionItemResource> items = new HashSet<MealPlanProjectionItemResource>();
 
@@ -16,6 +18,14 @@ public class MealPlanProjectionResource extends BaseResource {
 
     public String getFlight() {
         return this.flight;
+    }
+
+    public StationResource getUpliftStation() {
+        return this.upliftStation;
+    }
+
+    public StationResource getUnloadStation() {
+        return this.unloadStation;
     }
 
     public MealPlanResource getMealPlan() {
@@ -28,6 +38,14 @@ public class MealPlanProjectionResource extends BaseResource {
 
     public void setFlight(String flight) {
         this.flight = flight;
+    }
+
+    public void setUpliftStation(StationResource upliftStation) {
+        this.upliftStation = upliftStation;
+    }
+
+    public void setUnloadStation(StationResource unloadStation) {
+        this.unloadStation = unloadStation;
     }
 
     public void setMealPlan(MealPlanResource mealPlan) {
@@ -46,6 +64,14 @@ public class MealPlanProjectionResource extends BaseResource {
         final Object this$flight = this.flight;
         final Object other$flight = other.flight;
         if (this$flight == null ? other$flight != null : !this$flight.equals(other$flight)) return false;
+        final Object this$upliftStation = this.upliftStation;
+        final Object other$upliftStation = other.upliftStation;
+        if (this$upliftStation == null ? other$upliftStation != null : !this$upliftStation.equals(other$upliftStation))
+            return false;
+        final Object this$unloadStation = this.unloadStation;
+        final Object other$unloadStation = other.unloadStation;
+        if (this$unloadStation == null ? other$unloadStation != null : !this$unloadStation.equals(other$unloadStation))
+            return false;
         final Object this$mealPlan = this.mealPlan;
         final Object other$mealPlan = other.mealPlan;
         if (this$mealPlan == null ? other$mealPlan != null : !this$mealPlan.equals(other$mealPlan)) return false;
@@ -60,6 +86,10 @@ public class MealPlanProjectionResource extends BaseResource {
         int result = 1;
         final Object $flight = this.flight;
         result = result * PRIME + ($flight == null ? 0 : $flight.hashCode());
+        final Object $upliftStation = this.upliftStation;
+        result = result * PRIME + ($upliftStation == null ? 0 : $upliftStation.hashCode());
+        final Object $unloadStation = this.unloadStation;
+        result = result * PRIME + ($unloadStation == null ? 0 : $unloadStation.hashCode());
         final Object $mealPlan = this.mealPlan;
         result = result * PRIME + ($mealPlan == null ? 0 : $mealPlan.hashCode());
         final Object $items = this.items;
