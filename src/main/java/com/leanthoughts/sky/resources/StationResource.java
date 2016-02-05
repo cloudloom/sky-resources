5a02af683191e60430b4b207b3549a6028ff1870
Fahad Fazil <fahad.fazil007@gmail.com>
2016-02-05 19:17:25 +0530

    v2.5 Modified Station Resource

--- src/main/java/com/leanthoughts/sky/resources/StationResource.java
+++ src/main/java/com/leanthoughts/sky/resources/StationResource.java
@@ -3,9 +3,10 @@ package com.leanthoughts.sky.resources;
 /**
  * Created by fahad on 25-01-2016.
  */
+
 public class StationResource extends BaseResource {
 
-    private String Uid;
+    private String stationUid;
     private String code;
     private String name;
     private String airport;
@@ -15,91 +16,96 @@ public class StationResource extends BaseResource {
     public StationResource() {
     }
 
-    @Override
-    public String getUid() {
-        return Uid;
+    public String getStationUid() {
+        return stationUid;
     }
 
-    @Override
-    public void setUid(String uid) {
-        Uid = uid;
+    public void setStationUid(String stationUid) {
+        this.stationUid = stationUid;
     }
 
     public String getCode() {
-        return code;
+        return this.code;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getAirport() {
+        return this.airport;
+    }
+
+    public AirportResource getAirportDetails() {
+        return this.airportDetails;
+    }
+
+    public String getOrgUnitID() {
+        return this.orgUnitID;
     }
 
     public void setCode(String code) {
         this.code = code;
     }
 
-    public String getName() {
-        return name;
-    }
-
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getAirport() {
-        return airport;
     }
 
     public void setAirport(String airport) {
         this.airport = airport;
     }
 
-    public String getOrgUnitID() {
-        return orgUnitID;
+    public void setAirportDetails(AirportResource airportDetails) {
+        this.airportDetails = airportDetails;
     }
 
     public void setOrgUnitID(String orgUnitID) {
         this.orgUnitID = orgUnitID;
     }
 
-    public AirportResource getAirportDetails() {
-        return airportDetails;
-    }
-
-    public void setAirportDetails(AirportResource airportDetails) {
-        this.airportDetails = airportDetails;
-    }
-
-    @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-
-        StationResource that = (StationResource) o;
-
-        if (Uid != null ? !Uid.equals(that.Uid) : that.Uid != null) return false;
-        if (code != null ? !code.equals(that.code) : that.code != null) return false;
-        if (name != null ? !name.equals(that.name) : that.name != null) return false;
-        if (airport != null ? !airport.equals(that.airport) : that.airport != null) return false;
-        return orgUnitID != null ? orgUnitID.equals(that.orgUnitID) : that.orgUnitID == null;
-
+        if (o == this) return true;
+        if (!(o instanceof StationResource)) return false;
+        final StationResource other = (StationResource) o;
+        if (!other.canEqual((Object) this)) return false;
+        final Object this$code = this.code;
+        final Object other$code = other.code;
+        if (this$code == null ? other$code != null : !this$code.equals(other$code)) return false;
+        final Object this$name = this.name;
+        final Object other$name = other.name;
+        if (this$name == null ? other$name != null : !this$name.equals(other$name)) return false;
+        final Object this$airport = this.airport;
+        final Object other$airport = other.airport;
+        if (this$airport == null ? other$airport != null : !this$airport.equals(other$airport)) return false;
+        final Object this$airportDetails = this.airportDetails;
+        final Object other$airportDetails = other.airportDetails;
+        if (this$airportDetails == null ? other$airportDetails != null : !this$airportDetails.equals(other$airportDetails))
+            return false;
+        final Object this$orgUnitID = this.orgUnitID;
+        final Object other$orgUnitID = other.orgUnitID;
+        if (this$orgUnitID == null ? other$orgUnitID != null : !this$orgUnitID.equals(other$orgUnitID)) return false;
+        return true;
     }
 
-    @Override
     public int hashCode() {
-        int result = super.hashCode();
-        result = 31 * result + (Uid != null ? Uid.hashCode() : 0);
-        result = 31 * result + (code != null ? code.hashCode() : 0);
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        result = 31 * result + (airport != null ? airport.hashCode() : 0);
-        result = 31 * result + (orgUnitID != null ? orgUnitID.hashCode() : 0);
+        final int PRIME = 59;
+        int result = 1;
+        final Object $code = this.code;
+        result = result * PRIME + ($code == null ? 0 : $code.hashCode());
+        final Object $name = this.name;
+        result = result * PRIME + ($name == null ? 0 : $name.hashCode());
+        final Object $airport = this.airport;
+        result = result * PRIME + ($airport == null ? 0 : $airport.hashCode());
+        final Object $airportDetails = this.airportDetails;
+        result = result * PRIME + ($airportDetails == null ? 0 : $airportDetails.hashCode());
+        final Object $orgUnitID = this.orgUnitID;
+        result = result * PRIME + ($orgUnitID == null ? 0 : $orgUnitID.hashCode());
         return result;
     }
 
-    @Override
-    public String toString() {
-        return "StationResource{" +
-                "Uid='" + Uid + '\'' +
-                ", code='" + code + '\'' +
-                ", name='" + name + '\'' +
-                ", airport='" + airport + '\'' +
-                ", orgUnitID='" + orgUnitID + '\'' +
-                '}';
+    protected boolean canEqual(Object other) {
+        return other instanceof StationResource;
     }
+
 }
