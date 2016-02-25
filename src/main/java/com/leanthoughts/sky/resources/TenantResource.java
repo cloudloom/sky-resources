6e69f36b5210441e59c472a259bf1dd8346f98a1
sadath <ssmdrepo@gmail.com>
2016-02-25 16:25:30 +0530

    changed to v3.1.3

--- src/main/java/com/leanthoughts/sky/resources/TenantResource.java
+++ src/main/java/com/leanthoughts/sky/resources/TenantResource.java
@@ -1,5 +1,10 @@
 package com.leanthoughts.sky.resources;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(value = JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TenantResource {
     private String uid;
     private String name;
