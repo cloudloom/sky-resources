19c1b1dc98a4bb03a1c0b49801052082ed9c6d63
Fahad Fazil <fahad.fazil007@gmail.com>
2016-05-04 12:40:17 +0530

    Added SearchCriteriaResource

---
+++ src/main/java/com/leanthoughts/sky/resources/flight/SearchCriteriaResource.java
@@ -0,0 +1,39 @@
+package com.leanthoughts.sky.resources.flight;
+
+/**
+ * Created by fahad on 03-05-2016.
+ */
+public class SearchCriteriaResource {
+
+
+    private String key;
+    private String operation;
+    private Object value;
+
+    public SearchCriteriaResource(){}
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+}
+
