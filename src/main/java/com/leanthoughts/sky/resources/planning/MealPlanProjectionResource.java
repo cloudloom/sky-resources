9415d2fc1c718e3f1d7a24f2fb6596b91e2dafd4
sadath <ssmdrepo@gmail.com>
2016-05-16 19:38:20 +0530

    v3.3.6

--- src/main/java/com/leanthoughts/sky/resources/planning/MealPlanProjectionResource.java
+++ src/main/java/com/leanthoughts/sky/resources/planning/MealPlanProjectionResource.java
@@ -9,17 +9,16 @@ import java.util.Set;
  * Created by sadath on 25-Jan-2016.
  */
 public class MealPlanProjectionResource extends BaseResource {
-    private String flight;
+    private String flightSector;
     private StationResource upliftStation;
     private StationResource unloadStation;
-    private MealPlanResource mealPlan;
     private Set<MealPlanProjectionItemResource> items = new HashSet<MealPlanProjectionItemResource>();
 
     public MealPlanProjectionResource() {
     }
 
-    public String getFlight() {
-        return this.flight;
+    public String getFlightSector() {
+        return this.flightSector;
     }
 
     public StationResource getUpliftStation() {
@@ -30,16 +29,12 @@ public class MealPlanProjectionResource extends BaseResource {
         return this.unloadStation;
     }
 
-    public MealPlanResource getMealPlan() {
-        return this.mealPlan;
-    }
-
     public Set<MealPlanProjectionItemResource> getItems() {
         return this.items;
     }
 
-    public void setFlight(String flight) {
-        this.flight = flight;
+    public void setFlightSector(String flightSector) {
+        this.flightSector = flightSector;
     }
 
     public void setUpliftStation(StationResource upliftStation) {
@@ -48,10 +43,6 @@ public class MealPlanProjectionResource extends BaseResource {
 
     public void setUnloadStation(StationResource unloadStation) {
         this.unloadStation = unloadStation;
-    }
-
-    public void setMealPlan(MealPlanResource mealPlan) {
-        this.mealPlan = mealPlan;
     }
 
     public void setItems(Set<MealPlanProjectionItemResource> items) {
@@ -63,9 +54,10 @@ public class MealPlanProjectionResource extends BaseResource {
         if (!(o instanceof MealPlanProjectionResource)) return false;
         final MealPlanProjectionResource other = (MealPlanProjectionResource) o;
         if (!other.canEqual((Object) this)) return false;
-        final Object this$flight = this.flight;
-        final Object other$flight = other.flight;
-        if (this$flight == null ? other$flight != null : !this$flight.equals(other$flight)) return false;
+        final Object this$flightSector = this.flightSector;
+        final Object other$flightSector = other.flightSector;
+        if (this$flightSector == null ? other$flightSector != null : !this$flightSector.equals(other$flightSector))
+            return false;
         final Object this$upliftStation = this.upliftStation;
         final Object other$upliftStation = other.upliftStation;
         if (this$upliftStation == null ? other$upliftStation != null : !this$upliftStation.equals(other$upliftStation))
@@ -74,9 +66,6 @@ public class MealPlanProjectionResource extends BaseResource {
         final Object other$unloadStation = other.unloadStation;
         if (this$unloadStation == null ? other$unloadStation != null : !this$unloadStation.equals(other$unloadStation))
             return false;
-        final Object this$mealPlan = this.mealPlan;
-        final Object other$mealPlan = other.mealPlan;
-        if (this$mealPlan == null ? other$mealPlan != null : !this$mealPlan.equals(other$mealPlan)) return false;
         final Object this$items = this.items;
         final Object other$items = other.items;
         if (this$items == null ? other$items != null : !this$items.equals(other$items)) return false;
@@ -86,14 +75,12 @@ public class MealPlanProjectionResource extends BaseResource {
     public int hashCode() {
         final int PRIME = 59;
         int result = 1;
-        final Object $flight = this.flight;
-        result = result * PRIME + ($flight == null ? 0 : $flight.hashCode());
+        final Object $flightSector = this.flightSector;
+        result = result * PRIME + ($flightSector == null ? 0 : $flightSector.hashCode());
         final Object $upliftStation = this.upliftStation;
         result = result * PRIME + ($upliftStation == null ? 0 : $upliftStation.hashCode());
         final Object $unloadStation = this.unloadStation;
         result = result * PRIME + ($unloadStation == null ? 0 : $unloadStation.hashCode());
-        final Object $mealPlan = this.mealPlan;
-        result = result * PRIME + ($mealPlan == null ? 0 : $mealPlan.hashCode());
         final Object $items = this.items;
         result = result * PRIME + ($items == null ? 0 : $items.hashCode());
         return result;
